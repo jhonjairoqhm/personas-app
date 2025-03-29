f0a963ba06a64030402c431b8676223342148937
jhonjairoqhm <jhonjairo1234qh@gmail.com>
2025-03-29 09:32:21 -0500

    Commune arreglado

--- resources/views/comuna/edit.blade.php
+++ resources/views/comuna/edit.blade.php
@@ -1,7 +1,6 @@
 <!doctype html>
  <html lang="en">
- 
- <head>
+   <head>
      <!-- Required meta tags -->
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,51 +9,60 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
  
-     <title>Edit Municipality</title>
- </head>
- 
- <body>
+     <title>Edit Commune</title>
+   </head>
+   <body>
      <div class="container">
-         <h1>Edit Municipality</h1>
-         <form method="POST" action="{{ route('municipios.update', ['municipio' => $municipio->muni_codi]) }}">
+         <h1>Edit Commune</h1>
+         <form method="POST" action="{{ route('comunas.update', ['comuna' => $comuna->comu_codi]) }}">
              @method('put')
              @csrf
      
              <div class="mb-3">
-                 <label for="muni_codi" class="form-label">Id</label>
-                 <input type="text" class="form-control" id="muni_codi" name="muni_codi" disabled 
-                     value="{{ $municipio->muni_codi }}">
-                 <div class="form-text">Municipality Id.</div>
+                 <label for="codigo" class="form-label">Id</label>
+                 <input type="text" class="form-control" id="id" aria-describedby="codigoHelp" name="id"
+                     disabled="disabled" value="{{ $comuna->comu_codi }}">
+                 <div id="codigoHelp" class="form-text">Commune Id.</div>
              </div>
      
              <div class="mb-3">
-                 <label for="muni_nomb" class="form-label">Municipality Name</label>
-                 <input type="text" class="form-control" id="muni_nomb" placeholder="Municipality name"
-                     name="muni_nomb" value="{{ $municipio->muni_nomb }}" required>
+                 <label for="name" class="form-label">Commune</label>
+                 <input type="text" class="form-control" id="name" placeholder="Commune name"
+                     name="name" value="{{ $comuna->comu_nomb }}">
              </div>
      
-             <label for="department">Department:</label>
-             <select class="form-select" id="department" name="depa_codi" required>
+             <label for="municipality">Municipality:</label>
+             <select class="form-select" id="municipality" name="code" required>
                  <option selected disabled value="">Choose one...</option>
-                 @foreach ($departamentos as $departamento)
-                     <option value="{{ $departamento->depa_codi }}" 
-                         {{ $departamento->depa_codi == $municipio->depa_codi ? 'selected' : '' }}>
-                         {{ $departamento->depa_nomb }}
-                     </option>
+                 @foreach ($municipios as $municipio)
+                     @if ($municipio->muni_codi == $comuna->muni_codi)
+                         <option selected value="{{ $municipio->muni_codi }}">{{ $municipio->muni_nomb }}</option>
+                     @else
+                         <option value="{{ $municipio->muni_codi }}">{{ $municipio->muni_nomb }}</option>
+                     @endif
                  @endforeach
              </select>
      
              <div class="mt-3">
                  <button type="submit" class="btn btn-primary">Update</button>
-                 <a href="{{ route('municipios.index') }}" class="btn btn-warning">Cancel</a>
+                 <a href="{{ route('comunas.index') }}" class="btn btn-warning">Cancel</a>
              </div>
          </form>
      </div>
+     
+     
  
-     <!-- Bootstrap Bundle with Popper -->
+     <!-- Optional JavaScript; choose one of the two! -->
+ 
+     <!-- Option 1: Bootstrap Bundle with Popper -->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
      </script>
- </body>
  
+     <!-- Option 2: Separate Popper and Bootstrap JS -->
+     <!--
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+     -->
+   </body>
  </html>
