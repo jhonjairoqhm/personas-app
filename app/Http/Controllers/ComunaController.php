b886fa44d0cfaa019c7d89c676beb51fa7baa6ba
jhonjairoqhm <jhonjairo1234qh@gmail.com>
2025-03-29 08:36:16 -0500

    Commune Edit

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -1,115 +1,123 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use App\Models\Comuna;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-
-class ComunaController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     * 
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //$comunas = Comuna:: all();
-        $comunas = DB::table('tb_comuna')
-        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
-        ->select('tb_comuna.*', "tb_municipio.muni_nomb")
-        ->get();
-        return view('comuna.index', ['comunas' => $comunas]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $municipios = DB::table('tb_municipio')
-            ->orderBy('muni_nomb')
-            ->get();
-            return view('comuna.new', ['municipios' => $municipios]);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * 
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $comuna = new Comuna();
-        // $comuna->comu_codi = $request->id;
-        // El código de comuna es auto incremental
-        $comuna->comu_nomb = $request->name;
-        $comuna->muni_codi = $request->code;
-        $comuna->save();
-
-        $comunas = DB::table('tb_comuna')
-            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
-            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
-            ->get();
-        return view('comuna.index', ['comunas' => $comunas]);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        $comuna = Comuna::find($id);
-        $municipios = DB::table('tb_municipio')
-            ->orderBy('muni_nomb')
-            ->get();
-        
-        return view('comuna.edit', ['comuna' => $comuna, 'municipios' => $municipios]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $comuna = Comuna::find($id);
-
-        $comuna->comu_nomb = $request->name;
-        $comuna->muni_codi = $request->code;
-        $comuna->save();
-    
-        $comunas = DB::table('tb_comuna')
-            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
-            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
-            ->get();
-    
-        return view('comuna.index', ['comunas' => $comunas]);
-    }
-
-    /**
+ 
+ namespace App\Http\Controllers;
+ 
+ use App\Models\Comuna;
+ use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\DB;
+ 
+ class ComunaController extends Controller
+ {
+     /**
+      * Display a listing of the resource.
+      * 
+      * @return \Illuminate\Http\Response
+      */
+     public function index()
+     {
+         //$comunas = Comuna:: all();
+         $comunas = DB::table('tb_comuna')
+         ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+         ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+         ->get();
+         return view('comuna.index', ['comunas' => $comunas]);
+     }
+ 
+     /**
+      * Show the form for creating a new resource.
+      * @return \Illuminate\Http\Response
+      */
+     public function create()
+     {
+         $municipios = DB::table('tb_municipio')
+             ->orderBy('muni_nomb')
+             ->get();
+             return view('comuna.new', ['municipios' => $municipios]);
+     }
+ 
+     /**
+      * Store a newly created resource in storage.
+      * 
+      * @param \Illuminate\Http\Request $request
+      * @return \Illuminate\Http\Response
+      */
+     public function store(Request $request)
+     {
+         $comuna = new Comuna();
+         // $comuna->comu_codi = $request->id;
+         // El código de comuna es auto incremental
+         $comuna->comu_nomb = $request->name;
+         $comuna->muni_codi = $request->code;
+         $comuna->save();
+ 
+         $comunas = DB::table('tb_comuna')
+             ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+             ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+             ->get();
+         return view('comuna.index', ['comunas' => $comunas]);
+     }
+ 
+     /**
+      * Display the specified resource.
+      */
+     public function show(string $id)
+     {
+         //
+     }
+ 
+     /**
+      * Show the form for editing the specified resource.
+      * 
+      * @param int $id
+      * @return \Illuminate\Http\Response
+      */
+     public function edit(string $id)
+     {
+         //
+         $comuna = Comuna::find($id);
+         $municipios = DB::table('tb_municipio')
+             ->orderBy('muni_nomb')
+             ->get();
+             return view('comuna.edit', ['comuna' => $comuna, 'municipios' => $municipios]);
+     }
+ 
+     /**
+      * Update the specified resource in storage.
+      */
+     public function update(Request $request, string $id)
+     {
+         //
+         $comuna = Comuna::find($id);
+ 
+         $comuna->comu_nomb = $request->name;
+         $comuna->muni_codi = $request->code;
+         $comuna->save();
+ 
+         $comunas = DB::table('tb_comuna')
+             ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+             ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+             ->get();
+ 
+         return view('comuna.index', ['comunas' => $comunas]);
+     }
+ 
+     /**
      * Remove the specified resource from storage.
+      *
+     * @param int $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(string $id)
-    {
-        $comuna = Comuna::find($id);
-        $comuna->delete();
-    
-        $comunas = DB::table('tb_comuna')
-            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
-            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
-            ->get();
-    
-        return view('comuna.index', ['comunas' => $comunas]);
-    }
-}
+     public function destroy($id)
+     {
+         $comuna = Comuna::find($id);
+         $comuna->delete();
+ 
+         $comunas = DB::table('tb_comuna')
+             ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+             ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+             ->get();
+ 
+         return view('comuna.index', ['comunas' => $comunas]);
+     }
+ 
+ }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ComunaController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
-Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
-Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
-Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
-Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
-Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+ 
+ use Illuminate\Support\Facades\Route;
+ use App\Http\Controllers\ComunaController;
+ 
+ Route::get('/', function () {
+     return view('welcome');
+ });
+ 
+ Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
+ Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
+ Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
+ Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
+ Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
+ Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
+ Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
